21bb587784286d81d165c85f8e9c0bf3ab8fe28e
ashutosh negi <ashutoshnegi195@gmail.com>
2022-02-23 11:25:09 +0530

    message

--- frontend/components/FullProfile.tsx
+++ frontend/components/FullProfile.tsx
@@ -1,6 +1,12 @@
 import { Grid, GridItem } from "@chakra-ui/react";
+import { PlusSquareIcon } from '@chakra-ui/icons';
 
 const FullProfile = () => {
+
+  const postingNewDataHandler = () => {
+    
+  }
+
   return (
     <Grid
       h="100vh"
@@ -8,7 +14,7 @@ const FullProfile = () => {
       templateColumns="repeat(5, 1fr)"
       gap={4}
     >
-      <GridItem rowSpan={2} colSpan={4} bg="papayawhip">profile photo</GridItem>
+      <GridItem rowSpan={2} colSpan={4} bg="papayawhip"> <button onClick={postingNewDataHandler}><PlusSquareIcon fontSize='xx-large' sx={{ position: 'absolute' , top: '22%' , right: '22%'}}></PlusSquareIcon></button> </GridItem>
       <GridItem rowSpan={8} colSpan={1} bg="tomato">active users</GridItem>
       <GridItem rowSpan={6} colSpan={4} bg="tomato">your posts</GridItem>
       {/* <GridItem rowSpan={1} colSpan={2} bg="papayawhip" /> */}
